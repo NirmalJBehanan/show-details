33b36cd2672362083e14cb921f33ab25bb2019f3
NirmalJBehanan <nirmalbehanan93@gmail.com>
2026-05-08 05:41:46 +0530

    new project

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Button from '../button/Button'
 import '../../App.css'
 
-const Card = ({name,email,btnname}) => {
+const Card = ({ name, email, btnname }) => {
   return (
     <div className='card-container'>
-          <div className='card-details'>
-                <h1>{name}</h1>
-                <p>{email}</p>
-            <div className='btn'>
-               <Button  name={btnname}/>
-            </div>
-          </div>
+      <div className='card-details'>
+        <h1>{name}</h1>
+        <p>{email}</p>
+        <div className='btn'>
+          <Button name={btnname} />
+        </div>
+      </div>
 
     </div>
   )
